5fe168e0a0fb85eea143b89a4b7d4366f25cc339
João Araujo <j.joaosantos@outlook.com>
2021-08-25 19:48:54 -0300

    Project created

---
+++ src/test/java/com/byjohnjava/springsales/SpringSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byjohnjava.springsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
