7a802e3509675423947567dcd3831462b96d2aab
João Araujo <j.joaosantos@outlook.com>
2021-10-14 16:27:36 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/byjohnjava/springsales/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.byjohnjava.springsales.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.byjohnjava.springsales.entities.Order;
+import com.byjohnjava.springsales.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
