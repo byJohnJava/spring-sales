5dff69b1d3e8628186e4b53cb1e74f6f0d8b1906
João Araujo <j.joaosantos@outlook.com>
2021-11-16 17:40:49 -0300

    Product

---
+++ src/main/java/com/byjohnjava/springsales/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.byjohnjava.springsales.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.byjohnjava.springsales.entities.Product;
+import com.byjohnjava.springsales.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
